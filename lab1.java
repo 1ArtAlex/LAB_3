3b9e786098be1055b287f477dc3294b5c7789c79
1ArtAlex <91719015+1ArtAlex@users.noreply.github.com>
2021-12-07 18:51:54 +0300

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -1,19 +1,18 @@
 import java.util.regex.*;
 import java.util.Scanner;
-
 public class lab1 {
     public static void main(String args[]) {
         String pattern = "(0[1-9]|[12]\\d|3[01])/(0[1-9]|1[012])/(19|[2-9][0-9])\\d\\d";
         Pattern p = Pattern.compile(pattern);
         Scanner data = new Scanner(System.in);
-        System.out.println("Введите дату");
+        System.out.println("введите дату");
         String text = data.next();
         data.close();
         Matcher m = p.matcher(text);
         if (m.matches() == true) {
-            System.out.println("Данная строка является датой типа dd/mm/yyyy и соответствует заданному диапазону");
+            System.out.println("данная дата является датой типа dd/mm/yyyy");
         } else {
-            System.out.println("Данная строка НЕ является датой типа dd/mm/yyyy и НЕ соответствует заднному диапазону");
+            System.out.println("данная дата не является датой типа dd/mm/yyyy");
         }
     }
 }
